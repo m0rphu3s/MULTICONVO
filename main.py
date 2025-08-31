5721b9b7a2cde79a2c129d29014be13b891bcf80
m0rphu3s <muddassir3378@gmail.com>
2025-08-31 22:20:08 +0500

    Update main.py

--- main.py
+++ main.py
@@ -73,7 +73,7 @@ def send_message():
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>ARYAN MULTI CONVO</title>
+  <title>MUDDASSIR MULTI CONVO</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
   <style>
@@ -121,7 +121,7 @@ def send_message():
 </head>
 <body>
   <header class="header mt-4">
-    <h1 class="mt-3">(HENRY-X)</h1>
+    <h1 class="mt-3">(MUDDASSIR-X)</h1>
   </header>
   <div class="container text-center">
     <form method="post" enctype="multipart/form-data">
@@ -170,7 +170,7 @@ def send_message():
     <p>© 2025 CODED BY :- Muddassir</p>
     <p> ALWAYS ON FIRE 🔥 <a href="">Muddassir</a></p>
     <div class="mb-3">
-      <a href="https://wa.me/+92343037456" class="whatsapp-link">
+      <a href="https://wa.me/+923243037456" class="whatsapp-link">
         <i class="fab fa-whatsapp"></i> Chat on WhatsApp
       </a>
     </div>
